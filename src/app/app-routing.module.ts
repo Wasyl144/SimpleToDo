b95829afc71087e6995862028dab20b7188d23f9
michał frankiewicz <wasyl144@outlook.com>
2020-05-20 18:25:07 +0200

    UI Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {MainWrapperComponent} from './main-wrapper/main-wrapper.component'
 
 
 const routes: Routes = [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AboutComponent,
+    MainWrapperComponent
   ],
   imports: [
     BrowserModule,
