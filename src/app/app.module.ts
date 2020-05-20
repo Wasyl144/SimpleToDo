e3ea5098926e48c81f106a101494999d52f84c76
michał frankiewicz <wasyl144@outlook.com>
2020-05-20 19:13:37 +0200

    Router-handling and some UI improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutComponent } from './about/about.component';
 import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ListComponent } from './main-wrapper/list/list.component';
+import { FormComponent } from './main-wrapper/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     AboutComponent,
-    MainWrapperComponent
+    MainWrapperComponent,
+    PageNotFoundComponent,
+    ListComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
